55238d8fa40077ee48285b4e48599cc9125660ad
alksware <122390250+alksware@users.noreply.github.com>
2023-02-21 23:59:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,162 @@
-import datetime
-import time as tm
 import random
+import msvcrt
+import time as tm
 
-class Controller():
-    def __init__(self, tv_mode, voice_volume=0, channels=['FOX'], avaible_channel='FOX'):
-        timeXXX = datetime.datetime.now()
-        time = datetime.datetime.ctime(timeXXX)
-        print("Controller has been started:", time)
-        self.tv_mode = tv_mode
-        self.voice_volume = voice_volume
-        self.channels = channels
-        self.avaible_channel = avaible_channel
 
-    def change_voice_volume(self):
+class Kumanda():
+
+    def __init__(self, tv_durum="Kapalı", tv_ses=0, kanal_listesi=["Trt"], kanal="Trt", dil_ayarı="Türkçe",altyazı_destegi=["Türkçe"]):
+        print("Kumanda Oluşturuluyor...")
+
+        self.tv_ses = tv_ses
+
+        self.tv_durum = tv_durum
+
+        self.kanal_listesi = kanal_listesi
+
+        self.kanal = kanal
+
+        self.dil_ayarı = dil_ayarı
+
+        self.altyazı_destegi = altyazı_destegi
+
+    def sesi_azalt_artir(self):
+
         while True:
-            voice_volume_IN = input("Press '+' for high voice volume, Press '-' for incrase voice volume, for quit 'q'")
-            if (voice_volume_IN == '+'):
-                self.voice_volume += 1
-                print("Avaible volume:", self.voice_volume)
-            elif (voice_volume_IN == '-'):
-                self.voice_volume -= 1
-                print("Avaible volume:", self.voice_volume)
+            karakter = input("Azaltmak için '<' Artırmak İçin '>' Tamam ise 'q' ya basın")
+
+            if (karakter == "<"):
+                if (self.tv_ses != 0):
+                    self.tv_ses -= 1
+                    print("Ses:", self.tv_ses)
+            elif (karakter == ">"):
+                if (self.tv_ses != 32):
+                    self.tv_ses += 1
+                    print("Ses:", self.tv_ses)
             else:
-                print("Voice volume updated:", self.voice_volume)
+                print("Ses Güncellendi:", self.tv_ses)
                 break
 
-    def close_tv_mode(self):
-        print("TV closing...")
-        tm.sleep(1)
-        print("TV closed")
-        self.tv_mode = "turn off"
+    def tv_kapat(self):
+        print("Tv kapatılıyor.")
 
-    def open_tv_mode(self):
-        print("TV opening...")
-        tm.sleep(1.5)
-        print("TV opened")
-        self.tv_mode = "turn on"
+        self.tv_durum = "Kapalı"
 
-    def open_random_channel(self):
-        create_random_channel = random.randint(0, len(self.channels) - 1)
-        self.avaible_channel = self.channels[create_random_channel]
-        print("Avaible channel:", self.avaible_channel)
-
-    def append_new_channel(self, channel):
-        print("Channel added to list")
-        self.channels.append(channel)
+    def tv_aç(self):
+        print("Tv Açılıyor.")
+        self.tv_durum = "Açık"
 
     def __str__(self):
-        return
-        "TV MODE:{}\n TV VOLUME:{}\n CHANNELS LIST:{}\n AVAIBLE CHANNEL:{}".format(self.tv_mode, self.voice_volume,
-                                                                                   self.channels, self.avaible_channel)
+        return "Tv Durumu : {}\nSes: {}\nKanallar: {}\nŞu anki kanal: {}\n".format(self.tv_durum, self.tv_ses,
+                                                                                   self.kanal_listesi, self.kanal)
 
-    def show_avaible_channel_number(self):
-        return len(self.channels)
+    def __len__(self):
+        return len(self.kanal_listesi)
 
-controller = Controller()
-print("""---Progresses---
-    1.Turn on TV
-    2.Turn off TV
-    3.TV information
-    4.Learn channel number
-    5.Add channel
-    6.Change TV volume
-""")
+    def rastgele_kanal(self):
+        rastgele = random.randint(0, len(self.kanal_listesi) - 1)
+
+        self.kanal = self.kanal_listesi[rastgele]
+
+        print("Şu anki Kanal:", self.kanal)
+
+    def kanal_ekle(self, kanal):
+        print("Kanal Eklendi ", kanal)
+        self.kanal_listesi.append(kanal)
+
+    def dil_ayarı_degistir(self):
+        secenek = input("Lütfen istediğiniz dili seçiniz:")
+        self.dil_ayarı = secenek
+        print("Lütfen bekleyiniz...")
+        tm.sleep(1)
+        print("Dil ayarı güncellendi:",secenek,"yeni dil olarak seçildi")
+
+    def altyazı_secenegi_ekle(self):
+        secenek = input("Lütfen eklemek istediğiniz altyazı dilini seçiniz:")
+        print("Lütfen bekleyiniz... {}  indiriliyor...".format(secenek))
+        tm.sleep(2)
+        self.altyazı_destegi.append(secenek)
+        print("{} varsayılan altyazı dillerine eklendi".format(secenek))
+
+    def mevcut_dil_ayarlarını_goster(self):
+        print("Bilgilere erişiliyor...")
+        tm.sleep(2)
+        print("Mevcut dil:",self.dil_ayarı)
+        print("Mevcut altyazı seçenekleri:", self.altyazı_destegi)
+
+
+
+
+kumanda = Kumanda()
+print("""*******************
+
+Televizyon Uygulaması
+
+İşlemler ;
+
+1. Televizyonu Aç
+
+2. Televizyonu Kapat
+
+3. Televizyon Bilgileri
+
+4. Kanal Sayısını Öğrenme
+
+5. Kanal Ekle
+
+6. Rastgele Kanal'a Geç
+
+7. Sesi Azalt Ya da Artır
+
+8. Dil ayarı değiştir
+
+9. Altyazı seçeneği ekle
+
+10. Mevcut dil seçeneklerini öğrenme
+
+Çıkmak için 'q' ya basın.
+*******************""")
 
 while True:
-        progress = input("Press a progress:")
-        if (progress == "1"):
-            controller.open_tv_mode()
-        elif (progress == "2"):
-            controller.close_tv_mode()
-        elif (progress == "3"):
-            print(controller)
-        elif (progress == "4"):
-            controller.show_avaible_channel_number()
-        elif (progress == "5"):
-            new_channel = input("Channel name for add:")
-            controller.append_new_channel(new_channel)
-        else:
-            break
+
+    işlem = input("İşlemi Seçiniz:")
+    if (işlem == "q"):
+        print("Programdan Çıkılıyor...")
+        break
+    if (işlem == "1"):
+        kumanda.tv_aç()
+    elif (işlem == "2"):
+        kumanda.tv_kapat()
+    elif (işlem == "3"):
+        print(kumanda)
+    elif (işlem == "4"):
+        print("Kanal Sayısı: ", len(kumanda))
+    elif (işlem == "5"):
+        kanallar = input("Eklemek İstediğiniz Kanalları ',' ile ayırarak girin:")
+        eklenecekler = kanallar.split(",")
+        for i in eklenecekler:
+            kumanda.kanal_ekle(i)
+        print("Kanal Listesi Başarıyla Güncellendi.")
+    elif (işlem == "6"):
+        kumanda.rastgele_kanal()
+    elif (işlem == "7"):
+        kumanda.sesi_azalt_artir()
+    elif (işlem == "8"):
+        kumanda.dil_ayarı_degistir()
+    elif (işlem == "9"):
+        kumanda.altyazı_secenegi_ekle()
+    elif (işlem == "10"):
+        kumanda.mevcut_dil_ayarlarını_goster()
+    else:
+        print("Geçersiz İşlem...")
 
 
 
-            
+
+
+
+
+
 
 
 
